1ad08f20a78f76dd33c303229fda8a096a00f97f
Peter Carmichael <peter.carmichael22@gmail.com>
2019-10-14 13:49:23 -0700

    tests passing

--- __tests__/hello.test.js
+++ __tests__/hello.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const hello = require('../../src/hello');
+const hello = require('../src/hello');
 
 describe('Testing Hello Module', () => {
   it('works when given a string', () => {
@@ -10,7 +10,7 @@ describe('Testing Hello Module', () => {
   });
 
   it('returns null when given an array', () => {
-    let message = hello.sayHello(['Jabuc']);
+    let message = hello.sayHello(['Peter']);
     expect(message).toEqual(null);
   });
 });
